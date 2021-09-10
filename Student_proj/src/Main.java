536ff8eaebaa0906dde4495360c8677f827aa5cb
ssinghae <thsgo1357@naver.com>
2021-09-10 20:53:56 +0900

    first commit

---
+++ Student_proj/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	
+		List<Student> al=new ArrayList<Student>();
+		System.out.println("Student List (ordered by name) ");
+		Student s1=new Student(101,"Kim",23);
+		al.add(s1);
+		Student s2=new Student(102,"Choi",21);
+		al.add(s2);
+		al.add(new Student(103, "Park",25));
+		
+		
+		
+		Collections.sort(al);
+		
+		for(Student s : al) {
+			System.out.println(s.toString());
+			
+			
+		}
+		System.out.println("Student List (reverse ordered by name)");
+		
+		Collections.sort(al,Collections.reverseOrder());
+		
+		for (int i=0; i<al.size(); i++) {
+			System.out.println(al.get(i).toString());
+		}
+	
+		
+}
+}
